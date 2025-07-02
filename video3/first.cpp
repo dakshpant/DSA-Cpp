8ed29213e49f1651d96d29fab16826a34563a8cc
Daksh Pant <dakshpant15@gmail.com>
2025-07-02 20:39:26 +0530

    Basics

---
+++ video3/first.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // cout<<"Hello World";
+    // cout<<"Hello World"<<endl;
+
+    // VariableDeclare
+    int a;
+    a=10;
+    // cout<<a;
+
+    // Float
+    float b=2.13;
+    // cout<<b;
+    // double
+    double c= 3.12343;
+    // cout<<c;
+    
+
+}
