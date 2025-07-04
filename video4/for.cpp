078f204d4c1491e51498a2ca6234c04193676a4e
Daksh Pant <dakshpant15@gmail.com>
2025-07-04 09:48:03 +0530

    upto lecture 3

---
+++ video4/for.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int count = 10;
+    for (int i = 0; i < count; i++)
+    {
+        cout<<i<<endl;
+        /* code */
+    }
+    
+}
---
+++ video4/if-Else.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int a,b;
+    // cin>>a;
+    // cin>>b;
+    // cout<<"The sum of a and b is:"<<a+b;
+
+    int package;
+    cin>>package;
+    if (package>10)
+    {
+        cout<<"Accepted";
+    }else{
+        cout<<"Rejected";
+    }
+
+
+};
