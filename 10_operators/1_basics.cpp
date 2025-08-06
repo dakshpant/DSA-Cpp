76fd15e072cd0c4f2cb9877a2904366b79034004
Daksh Pant <dakshpant15@gmail.com>
2025-08-06 10:16:09 +0530

    Operators

---
+++ 10_operators/1_basics.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int a = 10;
+    // int b=a++;
+    // cout<<b<<" "<<a;          
+    // return 0;
+    // Logical Operators
+    // int a, b, c;
+    // cin>>a>>b>>c;
+
+    // if(a>b&&a>c)
+    // cout<<"Yes";
+    // else
+    // cout<<"no";
+    // or
+
+    char name;
+    cin>>name;
+    if(name=='a'||name=='e'||name=='i'||name=='o'||name=='u')
+    cout<<"Vowel";
+    else
+    cout<<"consonant"; 
+
+}
