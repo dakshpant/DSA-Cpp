1801204654d694060b8f4a36b14e3df296016123
Daksh Pant <dakshpant15@gmail.com>
2025-08-11 10:04:14 +0530

    Lecture 11 While, Do while, Switch case and scope of a Var

---
+++ 11_WhileDoWhile/1_while.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int n, i;
+    // cin >> n;
+    // i = 1;
+    // while (i <= 10)
+    // {
+    //     /* code */
+    //     cout << n * i<<" ";
+    //     i++;
+    // }
+
+    // Switch Case
+    int i;
+    cout<<"Enter The Input Number:";
+    cin>>i;
+    switch (i)
+    {
+    case 1:
+        /* code */
+        cout<<"Mohit";
+        break;
+        case 2:
+        cout<<"Rohit";
+        break;
+
+    default:
+    cout<<"Sohit";
+        break;
+    }
+
+    return 0;
+}
